59462c3fa85b07ce65b6380006f5d755d28e3a5a
Barghes1 <coderedxd03@gmail.com>
2025-06-26 11:00:13 +0300

    Server.js Changes

--- backend/server.js
+++ backend/server.js
@@ -43,12 +43,12 @@ app.use('/api/posts', posts);
 app.use('/api/comments', comments);
 app.use('/api/awards', awards);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   );
+// }
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
